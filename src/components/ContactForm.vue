6ef2ce14db93734e3393f9c693ba2ad39af1fbb7
Nguyen Hoang vinh <vinh@email.com>
2025-11-19 01:19:50 +0700

    Tao trang cap nhat lien he

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,42 +2,64 @@
   <Form @submit="submitContact" :validation-schema="contactFormSchema">
     <div class="form-group">
       <label for="name">Tên</label>
-      <Field name="name" type="text" class="form-control" v-model="contactLocal.name" />
+      <Field
+        name="name"
+        type="text"
+        class="form-control"
+        v-model="contactLocal.name"
+      />
       <ErrorMessage name="name" class="error-feedback" />
     </div>
-
     <div class="form-group">
       <label for="email">E-mail</label>
-      <Field name="email" type="email" class="form-control" v-model="contactLocal.email" />
+      <Field
+        name="email"
+        type="email"
+        class="form-control"
+        v-model="contactLocal.email"
+      />
       <ErrorMessage name="email" class="error-feedback" />
     </div>
-
     <div class="form-group">
       <label for="address">Địa chỉ</label>
-      <Field name="address" type="text" class="form-control" v-model="contactLocal.address" />
+      <Field
+        name="address"
+        type="text"
+        class="form-control"
+        v-model="contactLocal.address"
+      />
       <ErrorMessage name="address" class="error-feedback" />
     </div>
-
     <div class="form-group">
       <label for="phone">Điện thoại</label>
-      <Field name="phone" type="tel" class="form-control" v-model="contactLocal.phone" />
+      <Field
+        name="phone"
+        type="tel"
+        class="form-control"
+        v-model="contactLocal.phone"
+      />
       <ErrorMessage name="phone" class="error-feedback" />
     </div>
-
     <div class="form-group form-check">
-      <input name="favorite" type="checkbox" class="form-check-input" v-model="contactLocal.favorite" />
+      <input
+        name="favorite"
+        type="checkbox"
+        class="form-check-input"
+        v-model="contactLocal.favorite"
+      />
       <label for="favorite" class="form-check-label">
         <strong>Liên hệ yêu thích</strong>
       </label>
     </div>
-
     <div class="form-group">
-      <button class="btn btn-primary">Lưu</button>
-      <button v-if="contactLocal._id" type="button" class="ml-2 btn btn-danger" @click="deleteContact">
+      <button type="submit" class="btn btn-primary">Lưu</button>
+      <button
+        v-if="contactLocal._id"
+        type="button"
+        class="ml-2 btn btn-danger"
+        @click="deleteContact"
+      >
         Xóa
-      </button>
-      <button type="button" class="ml-2 btn btn-danger" @click="Cancel">
-        Thoát
       </button>
     </div>
   </Form>
@@ -46,7 +68,6 @@
 <script>
 import * as yup from "yup";
 import { Form, Field, ErrorMessage } from "vee-validate";
-
 export default {
   components: {
     Form,
@@ -55,7 +76,7 @@ export default {
   },
   emits: ["submit:contact", "delete:contact"],
   props: {
-    contact: { type: Object, required: true }
+    contact: { type: Object, required: true },
   },
   data() {
     const contactFormSchema = yup.object().shape({
@@ -76,11 +97,16 @@ export default {
           "Số điện thoại không hợp lệ."
         ),
     });
-
     return {
-      // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
-      // contactLocal để liên kết với các input trên form
-      contactLocal: this.contact,
+      contactLocal: this.contact
+        ? this.contact
+        : {
+            name: "",
+            email: "",
+            address: "",
+            phone: "",
+            favorite: false,
+          },
       contactFormSchema,
     };
   },
@@ -90,16 +116,6 @@ export default {
     },
     deleteContact() {
       this.$emit("delete:contact", this.contactLocal.id);
-    },
-    Cancel() {
-      const reply = window.confirm(
-        'You have unsaved changes! Do you want to leave?'
-      );
-
-      if (!reply) {
-        // stay on the page if user clicks 'Cancel'
-        return false;
-      } else this.$router.push({ name: "contactbook" });
     },
   },
 };
